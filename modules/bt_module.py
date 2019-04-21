8d2e52640ba2dd476c6ed22ec0bbd24e32f28db8
Everton Barreto <kakivis1@gmail.com>
2019-04-21 14:14:38 -0300

    Trying to send a message to a phone

--- modules/bt_module.py
+++ modules/bt_module.py
@@ -30,6 +30,9 @@ def lookUpNearbyBluetoothDevices():
     nearby_devices = bluetooth.discover_devices()
     for bdaddr in nearby_devices:
         print str(bluetooth.lookup_name(bdaddr)) + " [" + str(bdaddr) + "]"
+        if str(bluetooth.lookup_name(bdaddr)) == "kaks":
+            sendMessageTo(bdaddr)
 
 
 lookUpNearbyBluetoothDevices()
+
