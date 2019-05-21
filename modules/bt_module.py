cf04bc96d0450646a002cd75676ee2c569dddc0d
Everton Barreto <kakivis1@gmail.com>
2019-05-20 21:48:48 -0300

    moving files and combining examples into a possibly functional bt_module

--- modules/bt_module.py
+++ modules/bt_module.py
@@ -1,36 +1,176 @@
-from bluetooth import *
+import sys
+import dbus, dbus.mainloop.glib
+from gi.repository import GObject
+from example_advertisement import Advertisement
+from example_advertisement import register_ad_cb, register_ad_error_cb
+from example_gatt_server import Service, Characteristic
+from example_gatt_server import register_app_cb, register_app_error_cb
 
-server_sock = BluetoothSocket(RFCOMM)
-server_sock.bind(("", PORT_ANY))
-server_sock.listen(1)
+BLUEZ_SERVICE_NAME = 'org.bluez'
+DBUS_OM_IFACE = 'org.freedesktop.DBus.ObjectManager'
+LE_ADVERTISING_MANAGER_IFACE = 'org.bluez.LEAdvertisingManager1'
+GATT_MANAGER_IFACE = 'org.bluez.GattManager1'
+GATT_CHRC_IFACE = 'org.bluez.GattCharacteristic1'
+TROPICALIA_SERVICE_UUID = '6e400001-b5a3-f393-e0a9-e50e24dcca9e'
+TEST_CHARACTERISTIC_UUID = '6e400002-b5a3-f393-e0a9-e50e24dcca9e'
+TEST_ENCRYPT_CHARACTERISTIC_UUID = '6e400003-b5a3-f393-e0a9-e50e24dcca9e'
+TEST_SECURE_CHARACTERISTIC_UUID = '6e400004-b5a3-f393-e0a9-e50e24dcca9e'
+LOCAL_NAME = 'TropilaciaFreezer'
+mainloop = None
 
-port = server_sock.getsockname()[1]
 
-uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+class TestCharacteristic(Characteristic):
+	"""
+	Dummy test characteristic. Allows writing arbitrary bytes to its value, and
+	contains "extended properties", as well as a test descriptor.
 
-advertise_service(server_sock, "SampleServer",
-	service_id=uuid,
-	service_classes=[uuid, SERIAL_PORT_CLASS],
-	profiles=[SERIAL_PORT_PROFILE],
-	# protocols = [ OBEX_UUID ]
-)
+	"""
 
-print("Waiting for connection on RFCOMM channel %d" % port)
+	def __init__(self, bus, index, service):
+		Characteristic.__init__(
+			self, bus, index,
+			TEST_CHARACTERISTIC_UUID,
+			['read', 'write', 'writable-auxiliaries'],
+			service)
+		self.value = []
 
-client_sock, client_info = server_sock.accept()
-print("Accepted connection from ", client_info)
+	def ReadValue(self, options):
+		print('TestCharacteristic Read: ' + repr(self.value))
+		return self.value
 
-try:
-	while True:
-		data = client_sock.recv(1024)
-		if len(data) == 0:
-			break
-		print("received [%s]" % data)
-except IOError:
-	pass
+	def WriteValue(self, value, options):
+		print('TestCharacteristic Write: ' + repr(value))
+		self.value = value
 
-print("disconnected")
 
-client_sock.close()
-server_sock.close()
-print("all done")
+class TestEncryptCharacteristic(Characteristic):
+	"""
+	Dummy test characteristic requiring encryption.
+
+	"""
+
+	def __init__(self, bus, index, service):
+		Characteristic.__init__(
+			self, bus, index,
+			TEST_ENCRYPT_CHARACTERISTIC_UUID,
+			['encrypt-read', 'encrypt-write'],
+			service)
+		self.value = []
+
+	def ReadValue(self, options):
+		print('TestEncryptCharacteristic Read: ' + repr(self.value))
+		return self.value
+
+	def WriteValue(self, value, options):
+		print('TestEncryptCharacteristic Write: ' + repr(value))
+		self.value = value
+
+
+class TestSecureCharacteristic(Characteristic):
+	"""
+	Dummy test characteristic requiring secure connection.
+
+	"""
+
+	def __init__(self, bus, index, service):
+		Characteristic.__init__(
+			self, bus, index,
+			TEST_SECURE_CHARACTERISTIC_UUID,
+			['secure-read', 'secure-write'],
+			service)
+		self.value = []
+
+	def ReadValue(self, options):
+		print('TestSecureCharacteristic Read: ' + repr(self.value))
+		return self.value
+
+	def WriteValue(self, value, options):
+		print('TestSecureCharacteristic Write: ' + repr(value))
+		self.value = value
+
+
+class TropicaliaService(Service):
+	def __init__(self, bus, index):
+		Service.__init__(self, bus, index, TROPICALIA_SERVICE_UUID, True)
+		self.add_characteristic(TestCharacteristic(bus, 0, self))
+		self.add_characteristic(TestEncryptCharacteristic(bus, 1, self))
+		self.add_characteristic(TestSecureCharacteristic(bus, 2, self))
+
+class Application(dbus.service.Object):
+	def __init__(self, bus):
+		self.path = '/'
+		self.services = []
+		dbus.service.Object.__init__(self, bus, self.path)
+
+	def get_path(self):
+		return dbus.ObjectPath(self.path)
+
+	def add_service(self, service):
+		self.services.append(service)
+
+	@dbus.service.method(DBUS_OM_IFACE, out_signature='a{oa{sa{sv}}}')
+	def GetManagedObjects(self):
+		response = {}
+		for service in self.services:
+			response[service.get_path()] = service.get_properties()
+			chrcs = service.get_characteristics()
+			for chrc in chrcs:
+				response[chrc.get_path()] = chrc.get_properties()
+		return response
+
+class TropicaliaApplication(Application):
+	def __init__(self, bus):
+		Application.__init__(self, bus)
+		self.add_service(TropicaliaService(bus, 0))
+
+class TropicaliaAdvertisement(Advertisement):
+	def __init__(self, bus, index):
+		Advertisement.__init__(self, bus, index, 'peripheral')
+		self.add_service_uuid(TROPICALIA_SERVICE_UUID)
+		self.add_local_name(LOCAL_NAME)
+		self.include_tx_power = True
+
+def find_adapter(bus):
+	remote_om = dbus.Interface(bus.get_object(BLUEZ_SERVICE_NAME, '/'),
+		DBUS_OM_IFACE)
+	objects = remote_om.GetManagedObjects()
+	for o, props in objects.items():
+		for iface in (LE_ADVERTISING_MANAGER_IFACE, GATT_MANAGER_IFACE):
+			if iface not in props:
+				continue
+		return o
+	return None
+
+def main():
+	global mainloop
+	dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+	bus = dbus.SystemBus()
+	adapter = find_adapter(bus)
+	if not adapter:
+		print('BLE adapter not found')
+		return
+
+	service_manager = dbus.Interface(
+		bus.get_object(BLUEZ_SERVICE_NAME, adapter),
+		GATT_MANAGER_IFACE)
+	ad_manager = dbus.Interface(bus.get_object(BLUEZ_SERVICE_NAME, adapter),
+		LE_ADVERTISING_MANAGER_IFACE)
+
+	app = TropicaliaApplication(bus)
+	adv = TropicaliaAdvertisement(bus, 0)
+
+	mainloop = GObject.MainLoop()
+
+	service_manager.RegisterApplication(app.get_path(), {},
+		reply_handler=register_app_cb,
+		error_handler=register_app_error_cb)
+	ad_manager.RegisterAdvertisement(adv.get_path(), {},
+		reply_handler=register_ad_cb,
+		error_handler=register_ad_error_cb)
+	try:
+		mainloop.run()
+	except KeyboardInterrupt:
+		adv.Release()
+
+if __name__ == '__main__':
+	main()
