0d4f145551e6b11321c8e74f79669716c84dfaad
Everton Barreto <kakivis1@gmail.com>
2019-11-18 17:41:22 -0300

    Finishing implementation for event log and add code for manual testing

--- modules/event_logger/event_logger.py
+++ modules/event_logger/event_logger.py
@@ -14,7 +14,8 @@ class EventLogger:
 		events = self.get_events_from_file()
 		new_event = Event(self.event_id+1, timestamp, user_id, lunches)
 		events.append(new_event.__dict__)
-
+		self.save_event_log(events)
+		self.event_id = self.event_id + 1
 
 	def get_current_event_id(self):
 		return self.event_id
@@ -29,9 +30,11 @@ class EventLogger:
 			return 0
 
 	def get_events_from_file(self):
+		if not os.path.exists(self.file_name):
+			return []
 		file = open(self.file_name, "r")
 		contents = file.read()
-		events = json.loads(contents)["events"]
+		events = json.loads(contents)
 		file.close()
 		return events
 
@@ -41,3 +44,22 @@ class EventLogger:
 		file = open(self.file_name, 'w+')
 		json.dump(events, file)
 		file.close()
+
+	def get_storage_state(self):
+		events = self.get_events_from_file()
+		if not events:
+			exit("ERROR 01 - NO LOG FILE FOUND WHEN TRYING TO GET STORAGE STATE")
+		return events[-1]["lunches"]
+
+
+if __name__ == '__main__':
+	logger = EventLogger('log.txt')
+	# initialize log with full storage
+	logger.add_entry([8, 8, 8, 8], 00)
+	storage = logger.get_storage_state()
+	storage[3] -= 1
+	logger.add_entry(storage, 32)
+	storage[1] -= 2
+	logger.add_entry(storage, 2)
+	storage[0] -= 3
+	logger.add_entry(storage, 5)
